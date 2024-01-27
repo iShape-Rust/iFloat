60c279ad41af25897ad25e3a5da2dc4971157ab5
Nail Sharipov <nailxsharipov@gmail.com>
2024-01-27 10:56:25 +0300

    new fix_vec constructor

--- src/point.rs
+++ src/point.rs
@@ -1,12 +1,18 @@
 use serde::{Deserialize, Serialize};
+use crate::fix_vec::FixVec;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Point {
     pub x: i32,
     pub y: i32,
 }
+
 impl Point {
     pub fn new(x: i32, y: i32) -> Self {
         Self { x, y }
+    }
+
+    pub fn new_fix_vec(vec: FixVec) -> Self {
+        Self { x: vec.x as i32, y: vec.y as i32 }
     }
 }
