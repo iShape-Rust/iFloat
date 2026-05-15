abe19fdced2ddf0800cc6177f7d49b9faf223563
Nail Sharipov <nailxsharipov@gmail.com>
2026-05-15 22:22:13 +0300

    delete bin_key

--- src/int/number.rs
+++ src/int/number.rs
@@ -12,6 +12,7 @@ pub trait WideIntNumber:
     + Neg<Output = Self>
 {
     fn signum(self) -> Self;
+    fn from_f64(value: f64) -> Self;
 }
 
 impl WideIntNumber for i32 {
@@ -19,17 +20,32 @@ impl WideIntNumber for i32 {
     fn signum(self) -> Self {
         self.signum()
     }
+
+    #[inline(always)]
+    fn from_f64(value: f64) -> Self {
+        value as Self
+    }
 }
 impl WideIntNumber for i64 {
     #[inline(always)]
     fn signum(self) -> Self {
         self.signum()
     }
+
+    #[inline(always)]
+    fn from_f64(value: f64) -> Self {
+        value as Self
+    }
 }
 impl WideIntNumber for i128 {
     #[inline(always)]
     fn signum(self) -> Self {
         self.signum()
+    }
+
+    #[inline(always)]
+    fn from_f64(value: f64) -> Self {
+        value as Self
     }
 }
 pub trait IntNumber
@@ -45,16 +61,20 @@ where
         + Ord,
 {
     type Wide: WideIntNumber;
+    const BITS: u32;
     const MAX: Self;
     const MIN: Self;
     const ZERO: Self;
     const WIDE_ZERO: Self::Wide;
     fn wide(self) -> Self::Wide;
     fn from_usize(value: usize) -> Self;
+    fn from_f64(value: f64) -> Self;
+    fn to_f64(self) -> f64;
 }
 
 impl IntNumber for i16 {
     type Wide = i32;
+    const BITS: u32 = i16::BITS;
     const MAX: Self = Self::MAX;
     const MIN: Self = Self::MIN;
     const ZERO: Self = 0;
@@ -68,11 +88,22 @@ impl IntNumber for i16 {
     #[inline(always)]
     fn from_usize(value: usize) -> Self {
         value as Self
+    }
+
+    #[inline(always)]
+    fn from_f64(value: f64) -> Self {
+        (value + 0.5_f64.copysign(value)) as Self
+    }
+
+    #[inline(always)]
+    fn to_f64(self) -> f64 {
+        self as f64
     }
 }
 
 impl IntNumber for i32 {
     type Wide = i64;
+    const BITS: u32 = i32::BITS;
     const MAX: Self = Self::MAX;
     const MIN: Self = Self::MIN;
     const ZERO: Self = 0;
@@ -85,11 +116,22 @@ impl IntNumber for i32 {
     #[inline(always)]
     fn from_usize(value: usize) -> Self {
         value as Self
+    }
+
+    #[inline(always)]
+    fn from_f64(value: f64) -> Self {
+        (value + 0.5_f64.copysign(value)) as Self
+    }
+
+    #[inline(always)]
+    fn to_f64(self) -> f64 {
+        self as f64
     }
 }
 
 impl IntNumber for i64 {
     type Wide = i128;
+    const BITS: u32 = i64::BITS;
     const MAX: Self = Self::MAX;
     const MIN: Self = Self::MIN;
     const ZERO: Self = 0;
@@ -102,5 +144,15 @@ impl IntNumber for i64 {
     #[inline(always)]
     fn from_usize(value: usize) -> Self {
         value as Self
+    }
+
+    #[inline(always)]
+    fn from_f64(value: f64) -> Self {
+        (value + 0.5_f64.copysign(value)) as Self
+    }
+
+    #[inline(always)]
+    fn to_f64(self) -> f64 {
+        self as f64
     }
 }
