9d455dc65e5ab47b583a9175dc85a19814a8328f
Nail Sharipov <nailxsharipov@gmail.com>
2026-05-25 10:18:42 +0300

    clean api

--- src/triangle.rs
+++ src/triangle.rs
@@ -8,22 +8,22 @@ pub struct Triangle;
 impl Triangle {
     #[inline(always)]
     pub fn area_two<T: IntNumber>(p0: IntPoint<T>, p1: IntPoint<T>, p2: IntPoint<T>) -> T::Wide {
-        (p1 - p0).cross_product(p1 - p2)
+        (p1 - p0).cross_product(p2 - p0)
     }
 
-    #[inline(always)]
-    pub fn area<T: IntNumber>(p0: IntPoint<T>, p1: IntPoint<T>, p2: IntPoint<T>) -> T::Wide {
-        Self::area_two(p0, p1, p2) / T::Wide::TWO
-    }
+    // #[inline(always)]
+    // pub fn area<T: IntNumber>(p0: IntPoint<T>, p1: IntPoint<T>, p2: IntPoint<T>) -> T::Wide {
+    //     Self::area_two(p0, p1, p2) / T::Wide::TWO
+    // }
 
     #[inline(always)]
     pub fn is_clockwise<T: IntNumber>(p0: IntPoint<T>, p1: IntPoint<T>, p2: IntPoint<T>) -> bool {
-        Self::area_two(p0, p1, p2) > T::Wide::ZERO
+        Self::area_two(p0, p1, p2) < T::Wide::ZERO
     }
 
     #[inline(always)]
     pub fn is_cw_or_line<T: IntNumber>(p0: IntPoint<T>, p1: IntPoint<T>, p2: IntPoint<T>) -> bool {
-        Self::area_two(p0, p1, p2) >= T::Wide::ZERO
+        Self::area_two(p0, p1, p2) <= T::Wide::ZERO
     }
 
     #[inline(always)]
@@ -38,7 +38,7 @@ impl Triangle {
 
     #[inline(always)]
     pub fn clock_direction<T: IntNumber>(p0: IntPoint<T>, p1: IntPoint<T>, p2: IntPoint<T>) -> T::Wide {
-        Self::area_two(p0, p1, p2).signum()
+        Self::area_two(p0, p2, p1).signum()
     }
 
     #[inline]
@@ -91,25 +91,8 @@ impl Triangle {
         !(has_neg && has_pos) && q0 != T::Wide::ZERO && q1 != T::Wide::ZERO && q2 != T::Wide::ZERO
     }
 
-    #[inline]
-    pub fn is_not_contain_point<T: IntNumber>(
-        p: IntPoint<T>,
-        p0: IntPoint<T>,
-        p1: IntPoint<T>,
-        p2: IntPoint<T>,
-    ) -> bool {
-        let q0 = (p - p1).cross_product(p0 - p1);
-        let q1 = (p - p2).cross_product(p1 - p2);
-        let q2 = (p - p0).cross_product(p2 - p0);
-
-        let has_neg = q0 <= T::Wide::ZERO || q1 <= T::Wide::ZERO || q2 <= T::Wide::ZERO;
-        let has_pos = q0 >= T::Wide::ZERO || q1 >= T::Wide::ZERO || q2 >= T::Wide::ZERO;
-
-        has_neg && has_pos
-    }
-
     #[inline(always)]
     pub fn clock_order<T: IntNumber>(p0: IntPoint<T>, p1: IntPoint<T>, p2: IntPoint<T>) -> Ordering {
-        T::Wide::ZERO.cmp(&Self::area_two(p0, p1, p2))
+        Self::area_two(p0, p1, p2).cmp(&T::Wide::ZERO)
     }
 }
